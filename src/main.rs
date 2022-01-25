23023044f1e96c0e522d6928974d61b9bcd53bcd
Jamon Terrell <git@jamonterrell.com>
2022-01-24 18:28:51 -0600

    monotonic timer

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,82 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use cortex_m_rt::entry;
-use defmt::*;
+// use cortex_m_rt::entry;
 use defmt_rtt as _;
-use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
+// use rp_pico as bsp;
+// use rp_pico::pac::Interrupt;
+#[rtic::app(device = rp_pico::hal::pac, peripherals = true, dispatchers = [TIMER_IRQ_1])]
+mod app {
 
-// use bsp::hal::{
-//     clocks::{init_clocks_and_plls, Clock},
-//     pac,
-//     sio::Sio,
-//     watchdog::Watchdog,
-// };
-#[entry]
-fn main() -> ! {
-    info!("Program start");
-    // let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
+    use defmt::*;
+    use embedded_hal::digital::v2::OutputPin;
+    use rp2040_monotonic::*;
 
-    loop {
+    #[monotonic(binds = TIMER_IRQ_0, default = true)]
+    type MyMono = Rp2040Monotonic; // 100 Hz / 10 ms granularity
+
+    use rp_pico::{
+        hal::{self, clocks::init_clocks_and_plls, watchdog::Watchdog, Sio},
+        XOSC_CRYSTAL_FREQ,
+    };
+
+    #[shared]
+    struct Shared {
+        led: hal::gpio::Pin<hal::gpio::pin::bank0::Gpio25, hal::gpio::PushPullOutput>,
+    }
+
+    #[local]
+    struct Local {}
+
+    #[init]
+    fn init(c: init::Context) -> (Shared, Local, init::Monotonics) {
+        let mut resets = c.device.RESETS;
+        let mut watchdog = Watchdog::new(c.device.WATCHDOG);
+        let _clocks = init_clocks_and_plls(
+            XOSC_CRYSTAL_FREQ,
+            c.device.XOSC,
+            c.device.CLOCKS,
+            c.device.PLL_SYS,
+            c.device.PLL_USB,
+            &mut resets,
+            &mut watchdog,
+        )
+        .ok()
+        .unwrap();
+
+        let sio = Sio::new(c.device.SIO);
+        let pins = rp_pico::Pins::new(
+            c.device.IO_BANK0,
+            c.device.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut resets,
+        );
+        let mut led = pins.led.into_push_pull_output();
+        led.set_low().unwrap();
+
+        // let mut timer = hal::Timer::new(c.device.TIMER, &mut resets);
+        // let mut alarm = timer.alarm_0().unwrap();
+        // let _ = alarm.schedule(SCAN_TIME_US.microseconds());
+        // alarm.enable_interrupt(&mut timer);
+
+        let mono = rp2040_monotonic::Rp2040Monotonic::new(c.device.TIMER);
+        foo::spawn_after(1.secs()).unwrap();
+
+        (Shared { led }, Local {}, init::Monotonics(mono))
+    }
+    #[task(
+        shared = [led],
+        local = [tog: bool = true],
+    )]
+    fn foo(mut c: foo::Context) {
         info!("loop");
-        // delay.delay_ms(500);
 
-        // your code goes here
+        if *c.local.tog {
+            c.shared.led.lock(|l| l.set_high().unwrap());
+        } else {
+            c.shared.led.lock(|l| l.set_low().unwrap());
+        }
+        *c.local.tog = !*c.local.tog;
+        foo::spawn_after(1.secs()).unwrap();
     }
 }
